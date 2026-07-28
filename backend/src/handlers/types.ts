e165c949a5d75ee83647a9b31581403d0bea5f9d
Fransheska <fransheskaechevarria@gmail.com>
2026-07-28 11:17:00 +0300

    fix(backend): export HandlerFn and InputParserFn

--- backend/src/handlers/types.ts
+++ backend/src/handlers/types.ts
@@ -2,5 +2,4 @@ import { CommonEvent } from "commoneventframework/dist/types/commonEvent";
 import { APIGatewayProxyStructuredResultV2 } from "commoneventframework";
 
 export type InputParserFn = (event: CommonEvent) => any;
-
 export type HandlerFn = (input: any, event: CommonEvent) => Promise<APIGatewayProxyStructuredResultV2 | object>;
